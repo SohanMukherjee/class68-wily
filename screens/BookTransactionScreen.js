a31d80a5d0922c78e870f73b22944d45bcc87e5f
SohanMukherjee <73114040+SohanMukherjee@users.noreply.github.com>
2021-09-07 18:48:28 +0530

    Add files via upload

---
+++ screens/BookTransactionScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+
+export default class BookTranscationScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Issue or Return</Text>
+            </View>
+        )
+    }
+}
